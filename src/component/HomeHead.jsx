60c7d5f191cb0414713003a2a83cbada8fbdb66d
cjc <431909623@qq.com>
2023-07-12 10:53:01 +0800

    feat：完成首页头部

--- src/component/HomeHead.jsx
+++ src/component/HomeHead.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, {useMemo} from "react";
+import timg from "../assets/images/timg.jpg"
+import "./homeHead.less";
 
-const HomeHead = function() {
+const HomeHead = function(props) {
+  let {today} = props;
+  let time = useMemo(() => {
+    let [,month, day] = today.match(/^\d{4}(\d{2})(\d{2})$/),
+        area = ['', '一','二','三','四','五','六','七','八','九','十','十一','十二'];
+
+    return {
+      month: area[+month]+ '月',
+      day
+    }
+  }, [today]);
+
   return <div className="home-head-box">
-    首页头部
+    <div className="info">
+      <div className="date">
+        <span>{time.day}</span>
+        <span>{time.month}</span>
+      </div>
+      <div className="title">
+        知乎日报
+      </div>
+
+    </div>
+
+    <div className="picture">
+      <img src={timg} alt="" />
+    </div>
   </div>
 }
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
+import HomeHead from "../component/HomeHead";
+import _ from "../assets/utils"
 
 const Home = function() {
+  let [today, setToday] = useState(_.formatTime(null, "{0}{1}{2}"));
   return <div className="home-box">
-    首页
+    <HomeHead today={today} ></HomeHead>
+    
   </div>
 }
 
